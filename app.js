742affe156c1f7c8359ccada285488c804ba794f
leaderg <leader.g@gmail.com>
2019-07-06 06:39:17 +0000

    Creating jwt verification on express.

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ require('dotenv').config();
 const express= require('express');
 const jwt= require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const body_parser = require('body-parser');
+
+//Setting up App
 const App = express;
+App.use(body_parser.json());
 
 //Assigning sensitive data.
 const PORT = process.env.PORT; //Port
 const KEY = process.env.TOKEN; // Secret Key for jwt
+
 
 App.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
